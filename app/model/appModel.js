613f5d99165b9f34a2665b31a134e838a361f31f
Guillermo García López <memogl25@gmail.com>
2019-01-15 16:07:04 -0600

    remove Ticket class

--- app/model/appModel.js
+++ app/model/appModel.js
@@ -2,51 +2,49 @@
 
 const db = require('./db.js');
 
-class Ticket {
+const Ticket = function (ticket) {
+  this.id = ticket.id;
+  this.consecutivo = ticket.consecutivo;
+  this.esta_usado = ticket.esta_usado;
+};
 
-  constructor(ticket) {
-    this.id = ticket.id;
-    this.consecutivo = ticket.consecutivo;
-    this.esta_usado = ticket.esta_usado;
-  }
+Ticket.createTicket = (ticket, result) => {
+  db.query("INSERT INTO tickets set ?", ticket, (error, dbResponse) => {
+    if (error) {
+      console.log("error: ", error);
+      result(error, null);
+    } else {
+      console.log(dbResponse.insertId);
+      result(null, dbResponse.insertId);
+    }
+  });
+};
 
-  static createTicket(ticket, result) {
-    db.query("INSERT INTO tickets set ?", ticket, (error, dbResponse) => {
-      if (error) {
-        console.log("error: ", error);
-        result(error, null);
-      } else {
-        console.log(dbResponse.insertId);
-        result(null, dbResponse.insertId);
-      }
-    });
-  }
+Ticket.getTicketById = (ticketId, result) => {
+  db.query("Select * from tickets where id = ?", ticketId, (error, dbResponse) => {
+    if (error) {
+      console.log("error: ", error);
+      result(error, null);
+    } else {
+      result(null, dbResponse);
+    }
+  });
+};
 
-  static getTicketById(ticketId, result) {
-    db.query("Select * from tickets where id = ?", ticketId, (error, dbResponse) => {
-      if (error) {
-        console.log("error: ", error);
-        result(error, null);
-      } else {
-        result(null, dbResponse);
-      }
-    });
-  }
+Ticket.getAllTickets = (result) => {
+  db.query("Select * from tickets", (error, dbResponse) => {
+    if (error) {
+      console.log("error: ", error);
+      result(null, error);
+    } else {
+      console.log('tasks : ', dbResponse);
+      result(null, dbResponse);
+    }
+  });
+};
 
-  static getAllTickets(result) {
-    db.query("Select * from tickets", (error, dbResponse) => {
-      if (error) {
-        console.log("error: ", error);
-        result(null, error);
-      } else {
-        console.log('tasks : ', dbResponse);
-        result(null, dbResponse);
-      }
-    });
-  }
-
-  static updateById(id, ticket, result) {
-    db.query('UPDATE tickets SET consecutivo=?, esta_usado=? WHERE id=?',
+Ticket.updateById = (id, ticket, result) => {
+  db.query('UPDATE tickets SET consecutivo=?, esta_usado=? WHERE id=?',
     [ticket.consecutivo, ticket.esta_usado, id], (error, dbResponse) => {
       if (error) {
         console.log("error: ", error);
@@ -55,18 +53,17 @@ class Ticket {
         result(null, dbResponse);
       }
     });
-  }
+};
 
-  static remove(id, result) {
-    db.query('DELETE FROM tickets WHERE id=?', [id], (error, dbResponse) => {
-      if (error) {
-        console.log("error: ", error);
-        result(null, error);
-      } else {
-        result(null, dbResponse);
-      }
-    });  
-  }
-}
+Ticket.remove = (id, result) => {
+  db.query('DELETE FROM tickets WHERE id=?', [id], (error, dbResponse) => {
+    if (error) {
+      console.log("error: ", error);
+      result(null, error);
+    } else {
+      result(null, dbResponse);
+    }
+  });
+};
 
 module.exports = Ticket;
